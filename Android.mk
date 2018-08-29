6e4201c6d622622df641ef0de1849f34ff8b027f
Shekhawat2 <anandsingh215@yahoo.com>
2018-08-29 14:15:40 +0000

    kcufsettings

--- Android.mk
+++ Android.mk
@@ -33,6 +33,7 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
     android-support-v7-preference \
     android-support-v7-recyclerview \
     android-support-v14-preference \
+    android-support-design
 
 LOCAL_JAVA_LIBRARIES := \
     bouncycastle \
@@ -51,6 +52,7 @@ LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res \
     frameworks/support/v14/preference/res \
     frameworks/support/v7/appcompat/res \
     frameworks/support/v7/recyclerview/res \
+    frameworks/support/design/res \
     packages/apps/KCUFSettings/res
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
@@ -61,6 +63,7 @@ LOCAL_AAPT_FLAGS := --auto-add-overlay \
     --extra-packages android.support.v17.preference \
     --extra-packages android.support.v7.appcompat \
     --extra-packages android.support.v7.recyclerview \
+    --extra-packages android.support.design \
     --extra-packages com.kcuf.settings
 
 ifneq ($(INCREMENTAL_BUILDS),)
