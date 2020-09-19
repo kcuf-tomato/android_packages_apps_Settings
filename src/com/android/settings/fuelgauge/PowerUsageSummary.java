b16e6e7b7f4101a753982199267eb15a3d982d59
Sonal Singh <sonal.singh.19993@gmail.com>
2020-09-19 20:40:50 +0530

    Settings: Improve Battery Settings layout
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -413,7 +413,7 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
         // reload BatteryInfo and updateUI
         restartBatteryInfoLoader();
         updateLastFullChargePreference();
-        mScreenUsagePref.setSummary(StringUtil.formatElapsedTime(getContext(),
+        mScreenUsagePref.setSubtitle(StringUtil.formatElapsedTime(getContext(),
                 mBatteryUtils.calculateScreenUsageTime(mStatsHelper), false));
         mBatteryTemp.setSubtitle(
                 StagUtils.mccCheck(getContext()) ?
@@ -447,7 +447,7 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
             final long lastFullChargeTime = mBatteryUtils.calculateLastFullChargeTime(mStatsHelper,
                     System.currentTimeMillis());
             mLastFullChargePref.setTitle(R.string.battery_last_full_charge);
-            mLastFullChargePref.setSummary(
+            mLastFullChargePref.setSubtitle(
                     StringUtil.formatRelativeTime(getContext(), lastFullChargeTime,
                             false /* withSeconds */));
 //        }
