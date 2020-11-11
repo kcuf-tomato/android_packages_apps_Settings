554de5493d10efc5f191a84251777fe29d4e259f
maxwen <max.weninger@gmail.com>
2020-11-11 16:04:30 +0100

    Settings: Navigation mode settings [2/2]
    * Updated for 11
    
    Added:
    
    * show changes in height in the indicator too
    
    Change-Id: I4e8070f5bf58b7148f42717e82cb401b9c5c7be6
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -284,9 +284,9 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
         updateBatteryTipFlag(icicle);
 
         // Check availability of Battery Health
-        if (!getResources().getBoolean(R.bool.config_supportBatteryHealth)) {
-            getPreferenceScreen().removePreference(mBatteryInfoCat);
-        }
+        //if (!getResources().getBoolean(R.bool.config_supportBatteryHealth)) {
+        //    getPreferenceScreen().removePreference(mBatteryInfoCat);
+        //}
 
         // Check availability of Smart Charging
         Preference mSmartCharging = (Preference) findPreference("smart_charging_key");
@@ -420,15 +420,15 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
         mBatChgCyc = getResources().getString(R.string.config_batChargeCycle);
 
         // Check availability of Battery Health
-        Preference mDesignedHealthPref = (Preference) findPreference(KEY_DESIGNED_BATTERY_CAPACITY);
+        Preference mDesignedHealthPref = (PowerGaugePreference) findPreference(KEY_DESIGNED_BATTERY_CAPACITY);
         if (!getResources().getBoolean(R.bool.config_supportBatteryHealth)) {
             getPreferenceScreen().removePreference(mDesignedHealthPref);
         }
-        Preference mCurrentHealthPref = (Preference) findPreference(KEY_CURRENT_BATTERY_CAPACITY);
+        Preference mCurrentHealthPref = (PowerGaugePreference) findPreference(KEY_CURRENT_BATTERY_CAPACITY);
         if (!getResources().getBoolean(R.bool.config_supportBatteryHealth)) {
             getPreferenceScreen().removePreference(mCurrentHealthPref);
         }
-        Preference mCyclesHealthPref = (Preference) findPreference(KEY_BATTERY_CHARGE_CYCLES);
+        Preference mCyclesHealthPref = (PowerGaugePreference) findPreference(KEY_BATTERY_CHARGE_CYCLES);
         if (!getResources().getBoolean(R.bool.config_supportBatteryHealth)) {
             getPreferenceScreen().removePreference(mCyclesHealthPref);
         }
