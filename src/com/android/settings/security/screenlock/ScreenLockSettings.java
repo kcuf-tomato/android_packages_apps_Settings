30f5b011e9fd3b11826d11b588e16d5b3ca2dc80
vjspranav <pranavasri@live.in>
2021-01-14 16:45:46 +0100

    Revert "Settings: Add screen off fod [2/2]"
    This reverts commit 9d3802d5f1f39e575d6f09fa91a7489adb7f91fb.

--- src/com/android/settings/security/screenlock/ScreenLockSettings.java
+++ src/com/android/settings/security/screenlock/ScreenLockSettings.java
@@ -81,7 +81,6 @@ public class ScreenLockSettings extends DashboardFragment
                 context, MY_USER_ID, lockPatternUtils));
         controllers.add(new QuickUnlockPreferenceController(
                 context, MY_USER_ID, lockPatternUtils));
-        controllers.add(new ScreenOffFodPreferenceController(context));
         controllers.add(new OwnerInfoPreferenceController(context, parent));
         controllers.add(new ScramblePinPreferenceController(
                 context, MY_USER_ID, lockPatternUtils));
